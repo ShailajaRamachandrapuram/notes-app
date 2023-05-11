c8bbc5b3a43688eec7b44bc24409ee9865275b80
Shailaja <111623287+ShailajaRamachandrapuram@users.noreply.github.com>
2023-05-10 17:45:57 -0700

    Update Home.js

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -64,8 +64,8 @@ export default function Home() {
   function renderLander() {
     return (
       <div className="lander">
-        <h1>Scratch</h1>
-        <p className="text-muted">A simple note taking app</p>
+        <h1>A Simple Notes</h1>
+        <p className="text-muted">Welcome to A simple Notes</p>
       </div>
     );
   }
